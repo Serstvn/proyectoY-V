3f43896019bb0da81456f560da821c4fb56c011e
Serstvn <93801482+Serstvn@users.noreply.github.com>
2022-03-30 22:52:46 -0500

    index

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const proyectosC= require("../controlador/proyectosC");
 
 /* GET home page. */
 router.get('/',function(req,res,next){
-  res.send("ESTE ES EL INDEX");
+  res.render('index');
 });
 
 
