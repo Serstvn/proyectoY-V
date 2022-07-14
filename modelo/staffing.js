f757785a34d3e91f2f3fa9c4c223d74dffc5758c
Serstvn <93801482+Serstvn@users.noreply.github.com>
2022-07-14 14:50:09 -0500

    indexSTF completo

--- modelo/staffing.js
+++ modelo/staffing.js
@@ -3,7 +3,7 @@ module.exports = {
 
 
     obtener: function (conn, funcion) {
-        conn.query("SELECT doc.tipoDoc, g.gerencia, d.departamento, c.cargo,  ST.id, E.documento, E.nombre, E.apellido, P.codigo, cec.ceco ,P.cliente, P.nomCorto, DATE_FORMAT(fechaInicial, '%M %d %Y') AS fechaInicial,  DATE_FORMAT(fechaFinal, '%M %d %Y') AS fechaFinal, valor FROM staffing AS ST INNER JOIN empleados AS E ON ST.id_empleado=E.id INNER JOIN proyectos AS P ON ST.id_proyecto=P.id INNER JOIN documentos as doc on e.documentos_id=doc.id INNER JOIN gerencias as g on e.gerencias_id=g.id INNER JOIN departamentos as d on e.departamentos_id=d.id INNER JOIN cargos as c on e.cargos_id=c.id INNER JOIN cecos as cec on p.ceco_id=cec.id", funcion);
+        conn.query("SELECT  ST.id, DATE_FORMAT(fechaInicial, '%M %d %Y') AS fechaInicial,  DATE_FORMAT(fechaFinal, '%M %d %Y') AS fechaFinal,valor,codigo,cliente,nomCorto, E1.documento, E1.nombre as nombreEmpleado,E1.apellido,tipoDoc,gerencia,departamento,cargo,ceco,E2.nombre as gerenteEncargado FROM staffing as ST inner join proyectos as P1 on ST.id_proyecto=P1.id inner join empleados as E1 on ST.id_empleado=E1.id INNER JOIN documentos as doc on E1.documentos_id=doc.id INNER JOIN gerencias as g on E1.gerencias_id=g.id INNER JOIN departamentos as d on E1.departamentos_id=d.id INNER JOIN cargos as c on E1.cargos_id=c.id INNER JOIN cecos as cec on P1.ceco_id=cec.id INNER JOIN empleados as E2 on P1.empleados_id=E2.id order by id", funcion);
     },
     retornarDatosIdSTF: function (conn, id, funcion) {
         conn.query("SELECT * FROM staffing WHERE id=?", [id], funcion);
